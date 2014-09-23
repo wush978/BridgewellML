79a2e5de44e3b8827571b99e0e7a4fba481f1e7d
Wush Wu <wush@bridgewell.com>
2014-09-23 16:48:47 +0800

    fix it to be compiled successful

--- include/DenseMatrix.hpp
+++ include/DenseMatrix.hpp
@@ -10,29 +10,29 @@ class DenseMatrix : public Matrix<IndexType> {
 public:
 
   DenseMatrix(IndexType _ncol, IndexType _nrow)
-  : Matrix(_ncol, _nrow)
+  : Matrix<IndexType>(_ncol, _nrow)
   { }
   
   virtual ~DenseMatrix() { }
   
   virtual IndexType nfeature() const {
-    return ncol;
+    return Matrix<IndexType>::ncol;
   }
   
   virtual IndexType ninstance() const {
-    return nrow;
+    return Matrix<IndexType>::nrow;
   }
   
   virtual IndexType getFeatureItorBegin(IndexType instance_id) const {
-    return instance_id * ncol;
+    return instance_id * Matrix<IndexType>::ncol;
   }
   
   virtual IndexType getFeatureItorEnd(IndexType instance_id) const {
-    return (instance_id + 1) * ncol;
+    return (instance_id + 1) * Matrix<IndexType>::ncol;
   }
   
   virtual IndexType getFeatureId(IndexType feature_iterator) const {
-    return feature_iterator %% ncol;
+    return feature_iterator % Matrix<IndexType>::ncol;
   }
   
   virtual double getValue(IndexType feature_iterator) const = 0;
