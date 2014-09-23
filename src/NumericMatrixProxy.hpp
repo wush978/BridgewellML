aadcd6424fa840087c4ca2f46059e323d3cfa828
Wush Wu <wush@bridgewell.com>
2014-09-23 17:39:55 +0800

    update NumericMatrixProxy for Matrix

--- src/NumericMatrixProxy.hpp
+++ src/NumericMatrixProxy.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
 #include <Rcpp.h>
-#include "DenseMatrix.hpp"
+#include "Matrix.hpp"
 
-class NumericMatrixProxy : public FTPRL::DenseMatrix<int> {
+class NumericMatrixProxy : public FTPRL::Matrix<int, size_t> {
+  
+  typedef int IndexType;
+  
+  typedef size_t ItorType;
   
   const double *data;
   
 public:
   
   NumericMatrixProxy(Rcpp::NumericMatrix src)
-  : DenseMatrix<int>(src.ncol(), src.nrow()), data(REAL(Rcpp::wrap(src)))
+  : Matrix<IndexType, ItorType>(src.ncol(), src.nrow()), data(REAL(Rcpp::wrap(src)))
   { }
   
   virtual ~NumericMatrixProxy() { }
   
-  virtual double getValue(int feature_iterator) const {
-    int instance_id = feature_iterator / FTPRL::Matrix<int>::ncol;
-    int feature_id = feature_iterator % FTPRL::Matrix<int>::ncol;
-    return data[feature_id * nrow + instance_id];
+  virtual ItorType getFeatureItorBegin(IndexType instance_id) const {
+    return instance_id * nfeature();
+  }
+  
+  virtual ItorType getFeatureItorEnd(IndexType instance_id) const {
+    return (instance_id + 1) * nfeature();
+  }
+  
+  virtual IndexType getFeatureId(ItorType feature_iterator) const {
+    return feature_iterator % nfeature();
+  }
+  
+  virtual double getValue(ItorType feature_iterator) const {
+    int instance_id = feature_iterator / nfeature();
+    int feature_id = feature_iterator % nfeature();
+    return data[feature_id * ninstance() + instance_id];
   }
   
 };
