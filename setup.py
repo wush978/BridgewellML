2dc4a655005f7a366d2a1e02f29974cc9073ad39
Wush Wu <wush@bridgewell.com>
2014-09-26 17:19:15 +0800

    extract version from changelog

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ extensions = [
         extra_compile_args = ['-fopenmp', '-std=c++11'],
         extra_link_args = ['-fopenmp']),
 ]
+
+with open("CHANGELOG", "r") as f:
+    version = f.readline().rstrip()
+
 setup(
     name = "FTPRL",
+    version = version,
     ext_modules = cythonize(extensions),
     install_requires = [
       "numpy",
