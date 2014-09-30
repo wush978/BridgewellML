59a3580545e88b52cad4db4e988ca26d421d0110
Wush Wu <wush@bridgewell.com>
2014-09-30 12:18:26 +0800

    refactor: put the type in argument implement: update and predict

--- src/logisticregression.pyx
+++ src/logisticregression.pyx
@@ -7,14 +7,15 @@ cdef extern from "LogisticRegression.hpp" namespace "FTPRL":
     cdef cppclass LogisticRegression[IndexType]:
         LogisticRegression(ftprl.FTPRL*, IndexType)
         void update[ItorType, LabelType](matrix.Matrix[IndexType, ItorType]* m, LabelType* y)
+        void predict[ItorType](matrix.Matrix[IndexType, ItorType]* m, double* y)
 
 cdef class PyLogisticRegression:
     cdef LogisticRegression[matrix.cINT32]* thisptr
-    def __cinit__(self, pyftprl, nfeature):
-        cdef matrix.cINT32 _nfeature = nfeature
-        cdef ftprl.FTPRL* p = (<ftprl.PyFTPRL?>pyftprl).thisptr
-        self.thisptr = new LogisticRegression[matrix.cINT32](p, _nfeature)
+    def __cinit__(self, ftprl.PyFTPRL pyftprl not None, matrix.cINT32 nfeature):
+        self.thisptr = new LogisticRegression[matrix.cINT32](pyftprl.thisptr, nfeature)
     def __dealloc__(self):
         del self.thisptr
     def update(self, matrix.PyScipySparseCSRMatrixProxy py_csr_matrix not None,  matrix.np.ndarray[matrix.cINT32, ndim = 1] py_y not None):
         self.thisptr.update[long, matrix.cINT32](py_csr_matrix.thisptr, &py_y[0])
+    def predict(self, matrix.PyScipySparseCSRMatrixProxy py_csr_matrix not None, matrix.np.ndarray[matrix.cDOUBLE, ndim = 1] py_y not None):
+        self.thisptr.predict[long](py_csr_matrix.thisptr, &py_y[0])
